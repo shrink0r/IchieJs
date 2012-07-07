d1caf50aabf8379a2a6c9fa76a9471f015a8c56e
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-07-07 00:39:04 +0000

    started ResizeInterActionHandler implementation. This guy is responseable for providing resize event for a selection rect, thereby providing usefull data such as position deltas and special key information etc.

--- pub/js/Ichie.SelectionRect.js
+++ pub/js/Ichie.SelectionRect.js
@@ -22,6 +22,8 @@ Ichie.SelectionRect = function(stage, options)
     this.crop_rect = null;
     
     this.initLayer();
+
+    this.resizeInteraction = new Ichie.ResizeInteractionHandler(this);
 };
 
 Ichie.SelectionRect.prototype.initLayer = function()
@@ -38,9 +40,9 @@ Ichie.SelectionRect.prototype.initLayer = function()
         y: (this.stage.getHeight() / 2) - (this.options.height / 2)
     });
     this.shapes_group.add(this.crop_rect);
-    for (var name in this.resize_handles)
+    for (var idx = 0; idx < this.resize_handles.length; idx++)
     {
-        this.shapes_group.add(this.resize_handles[name]);
+        this.shapes_group.add(this.resize_handles[idx]);
     }
     this.applyCropRectDragBounds();
     this.layer = new Kinetic.Layer();
@@ -87,69 +89,47 @@ Ichie.SelectionRect.prototype.createCropRect = function()
 Ichie.SelectionRect.prototype.repositionResizeHandles = function()
 {
     var that = this;
-    var handle_defs = Ichie.SelectionRect.HANDLES;
     var coord_map = this.buildResizeHandleCoordMap();
-    for (var handle_id in handle_defs)
+    for (var idx = 0; idx < Ichie.SelectionRect.HANDLES.length; idx++)
     {
-        var handle = this.shapes_group.get('#'+handle_id)[0];
-        var handle_coords = handle_defs[handle_id];
-        handle.setX(coord_map[handle_coords.x]);
-        handle.setY(coord_map[handle_coords.y]);
+        var handle_def = Ichie.SelectionRect.HANDLES[handle_id];
+        var handle = this.shapes_group.get('#'+handle_def.name)[0];
+        handle.setX(coord_map[handle_def.x]);
+        handle.setY(coord_map[handle_def.y]);
     }
 };
 
 Ichie.SelectionRect.prototype.createResizeHandles = function()
 {
     var that = this;
-    var resize_handles = {};
-    var handle_defs = Ichie.SelectionRect.HANDLES;
-    for (var handle_id in handle_defs)
+    var resize_handles = [];
+    for (var idx = 0; idx < Ichie.SelectionRect.HANDLES.length; idx++)
     {
-        resize_handles[handle_id] = that.createResizeHandle(
-            handle_id, 
-            handle_defs[handle_id]
+        resize_handles.push(
+            this.createResizeHandle(idx)
         );
     }
     return resize_handles;
 };
 
-Ichie.SelectionRect.prototype.createResizeHandle = function(handle_name, pos)
+Ichie.SelectionRect.prototype.createResizeHandle = function(idx)
 {
+    var handle_def = Ichie.SelectionRect.HANDLES[idx];
+    var coord_map = this.buildResizeHandleCoordMap();
     var reisze_handle_opts = {
       width: this.options.size,
       height: this.options.size,
       fill: this.options.fill,
       stroke: this.options.stroke,
-      strokeWidth: this.options.stroke_width,
-      draggable: true
+      strokeWidth: this.options.stroke_width
     };
-    var coord_map = this.buildResizeHandleCoordMap();
-    var rect = new Kinetic.Rect(
+    return new Kinetic.Rect(
         $.extend(
             {},
             reisze_handle_opts,
-            { id: handle_name, x: coord_map[pos.x], y: coord_map[pos.y] }
+            { id: handle_def.id, x: coord_map[handle_def.x], y: coord_map[handle_def.y] }
         )
     );
-    var that = this;
-    rect.on("mousedown touchstart", function() {
-        that.shapes_group.setDraggable(false);
-    });
-    rect.on("dragstart", function() {
-        rect.setDragBounds(
-            that.buildResizeHandleDragBounds(handle_name)
-        );
-    });
-    rect.on("dragmove", function(event) {
-        that.onResizeHandleMoved(event, rect);
-    });
-    rect.on("dragend", function() {
-        that.shapes_group.setDraggable(true);
-        rect.setDragBounds(
-            that.buildResizeHandleDragBounds('northWest')
-        );
-    });
-    return rect;
 };
 
 Ichie.SelectionRect.prototype.buildResizeHandleCoordMap = function()
@@ -164,97 +144,10 @@ Ichie.SelectionRect.prototype.buildResizeHandleCoordMap = function()
     };
 };
 
-Ichie.SelectionRect.prototype.buildResizeHandleDragBounds = function(handle_name)
-{
-    var handle_rect = this.shapes_group.get('#'+handle_name)[0];
-    var pos = handle_rect.getAbsolutePosition();
-    var img = this.stage.get('#src-img')[0];
-    var img_pos = img.getAbsolutePosition();
-    var center_offset = (this.options.size / 2);
-    var vertical_boundry = {
-        left: pos.x, right: pos.x,
-        top: img_pos.y - center_offset, 
-        bottom: img_pos.y + img.getHeight() - center_offset
-    };
-    var horizontal_boundry = {
-        top: pos.y, bottom: pos.y,
-        left: img_pos.x - center_offset, 
-        right: img_pos.x + img.getWidth() - center_offset
-    };
-    var img_boundry = {
-        top: img_pos.y - center_offset, 
-        bottom: img_pos.y + img.getHeight() - center_offset,
-        left: img_pos.x - center_offset, 
-        right: img_pos.x + img.getWidth() - center_offset
-    };
-    var boundry_map = {
-        'northWest': img_boundry,
-        'north': vertical_boundry,
-        'northEast': img_boundry,
-        'west': horizontal_boundry,
-        'east': horizontal_boundry,
-        'southWest': img_boundry,
-        'south': vertical_boundry,
-        'southEast': img_boundry
-    };
-    return boundry_map[handle_name];
-};
-
 Ichie.SelectionRect.prototype.onResizeHandleMoved = function(event, handle_rect)
 {
-    var pos = handle_rect.getPosition();
-    var handle_id = handle_rect.getId();
-    var center_offset = (this.options.size / 2);
-    var handle_center_x = pos.x + center_offset;
-    var handle_center_y = pos.y + center_offset;
-    var handle_abspos = handle_rect.getAbsolutePosition();
-    var croprect_relpos = this.crop_rect.getPosition();
-
-    if (/east$/ig.test(handle_id))
-    {
-        this.crop_rect.setWidth(handle_center_x - croprect_relpos.x);
-    }
-    if (/west$/ig.test(handle_id))
-    {
-        this.crop_rect.setWidth(this.crop_rect.getWidth() + croprect_relpos.x - handle_center_x);
-        this.shapes_group.setX(
-            this.shapes_group.getAbsolutePosition().x
-            + handle_abspos.x
-            - this.crop_rect.getAbsolutePosition().x + center_offset
-        );
-    }
-    if (/^north/ig.test(handle_id))
-    {
-        this.crop_rect.setHeight(this.crop_rect.getHeight() + croprect_relpos.y - handle_center_y);
-        this.shapes_group.setY(
-            this.shapes_group.getAbsolutePosition().y 
-            + handle_abspos.y 
-            - this.crop_rect.getAbsolutePosition().y + center_offset
-        );
-    }
-    if (/^south/ig.test(handle_id))
-    {
-        this.crop_rect.setHeight(handle_center_y - croprect_relpos.y);
-    }
 
 
-    if (this.options.keep_ratio)
-    {
-        if (event.mozMovementX > event.mozMovementY)
-        {
-            this.crop_rect.setHeight(
-                this.crop_rect.getWidth() / this.ratio
-            );
-        }
-        else
-        {
-            this.crop_rect.setWidth(
-                this.crop_rect.getHeight() * this.ratio
-            );
-        }
-    }
-
-    this.applyCropRectDragBounds();
     this.repositionResizeHandles();
     this.layer.draw();
 };
@@ -265,13 +158,19 @@ Ichie.SelectionRect.prototype.draw = function()
     this.layer.draw();
 };
 
-Ichie.SelectionRect.HANDLES = handle_defs = { 
-    'northWest': { x: 'west', y: 'north' },
-    'north': { x: 'center', y: 'north' },
-    'northEast': { x: 'east', y: 'north' },
-    'west': { x: 'west', y: 'middle' },
-    'east': { x: 'east', y: 'middle' },
-    'southWest': { x: 'west', y: 'south' },
-    'south': { x: 'center', y: 'south' },
-    'southEast': { x: 'east', y: 'south' }
+Ichie.SelectionRect.prototype.getHandles = function(idx)
+{
+    return this.resize_handles;
 };
+
+
+Ichie.SelectionRect.HANDLES = [
+    { id: 'northWest', x: 'west', y: 'north' },
+    { id: 'north', x: 'center', y: 'north' },
+    { id: 'northEast', x: 'east', y: 'north' },
+    { id: 'west', x: 'west', y: 'middle' },
+    { id: 'east', x: 'east', y: 'middle' },
+    { id: 'southWest', x: 'west', y: 'south' },
+    { id: 'south', x: 'center', y: 'south' },
+    { id: 'southEast', x: 'east', y: 'south' }
+];
