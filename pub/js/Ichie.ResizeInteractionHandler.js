c324524fe0b670b0c71798539e8e4cd2398a7df5
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-07-07 01:11:00 +0000

    actually added underscore (I'm still a bloody git noob) js lib and so on ... see prev commit message :)

---
+++ pub/js/Ichie.ResizeInteractionHandler.js
@@ -0,0 +1,42 @@
+/**
+ * #################################################################################
+ * #    Handles propagtion of resize events along with some precalculated data,    #
+ * #    that provides a bit convenience when handling mousemove events             #
+ * #    in the context of resizing our "SelectionRect".                            #
+ * #################################################################################
+ */
+Ichie.ResizeInteractionHandler = function(selection_rect)
+{
+    this.selection_rect = selection_rect;
+    this.handles = this.selection_rect.getHandles();
+    this.last_mousepos = null;
+    
+    this.registerHandleEvents();
+};
+
+Ichie.ResizeInteractionHandler.prototype.registerHandleEvents = function()
+{
+    for (var that = this, i = 0; i < this.handles.length; i++)(function(index)
+    {
+        var handle = that.handles[index];
+        var mouseMoveHandler = function(){ that.onMouseMove(index); };
+        handle.on('mousedown touchstart', function(event)
+        {
+            that.active_handle_idx = index;
+            // @todo set this.last_mousepos
+            window.document.addEventListener('mousemove', mouseMoveHandler);
+        });
+        handle.on('mouseup touchend', function()
+        {
+            that.active_handle_idx = null;
+            window.document.removeEventListener('mousemove', mouseMoveHandler);
+        });
+    })(i);
+};
+
+Ichie.ResizeInteractionHandler.prototype.onMouseMove = function(handle_index)
+{
+    var handle = this.handles[handle_index];
+    console.log("YAY MOUSE MOVED: " + handle_index);
+    // @todo Calc delta stuff.
+};
