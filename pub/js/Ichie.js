18e71c2de293927465a614a47b500fa26e881c8d
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-07-10 22:39:36 +0000

    Fixed last issues with the first draft implementation of the LockedRatioMode.

--- pub/js/Ichie.js
+++ pub/js/Ichie.js
@@ -516,7 +516,7 @@
                 delta = this.calculateEventDelta(handle_index, evt_pos);
 
             this.image_selection.setSelection(
-                this.mode.apply(handle_index, delta)
+                this.mode.buildSelectGeometry(handle_index, delta)
             );
 
             this.last_mousepos = evt_pos;
@@ -606,7 +606,7 @@
         /**
          * Get the rect resize/reposition calcultions done for 'default' selections.
          */
-        apply: function(handle_index, delta)
+        buildSelectGeometry: function(handle_index, delta)
         {
             return this.clipSelectionToBoundry({
                 pos: this.calculateSelectPostion(handle_index, delta),
@@ -731,7 +731,7 @@
      */
     var LockedRatioMode = function()
     {
-        DefaultMode.prototype.apply.constructor.call(this);
+        DefaultMode.prototype.constructor.call(this);
     };
     LockedRatioMode.prototype = new DefaultMode();
     LockedRatioMode.prototype.constructor = LockedRatioMode;
@@ -739,9 +739,9 @@
     /**
      * Get the rect resize/reposition calcultions done for 'locked-ration' selections.
      */
-    LockedRatioMode.prototype.apply = function(handle_index, delta)
+    LockedRatioMode.prototype.buildSelectGeometry = function(handle_index, delta)
     {
-        var selection = DefaultMode.prototype.apply.call(this, handle_index, delta); // luke, I am your father!
+        var selection = DefaultMode.prototype.buildSelectGeometry.call(this, handle_index, delta); // luke, I am your father!
 
         var image_selection = this.interaction.getImageSelection(),
             dir = ResizeInteraction.DIRECTION,
@@ -752,51 +752,66 @@
             selection_rect = image_selection.getSelectionRect(),
             selection_pos = selection_rect.getAbsolutePosition();
 
-        // Calculate the position and dimensions depending on the given ration.
         var reposition_x = [0, 6], reposition_y = [0, 2]; // handles that require repositioning of the select rect
         if (direction === dir.HORIZONTAL)
         {
-            var new_height = dimensions.width / image_selection.ratio;
-            if (-1 !== reposition_y.indexOf(handle_index))
+            if (position.y + dimensions.height >= boundry.bottom 
+                && (3 <= handle_index && 7 >= handle_index)
+                && selection_rect.getWidth() <= dimensions.width)
             {
-                position.y = selection_pos.y - (new_height - selection_rect.getHeight());
+                dimensions.height = boundry.bottom - position.y;
+                dimensions.width = dimensions.height * image_selection.ratio;
+                position.x = selection_pos.x;
             }
-            dimensions.height = new_height;
+            else if (position.y <= boundry.top 
+                && (0 <= handle_index && 2 >= handle_index)
+                && selection_rect.getWidth() <= dimensions.width)
+            {
+                var bottom_bound = position.y + dimensions.height;
+                position.y = boundry.top;
+                dimensions.height = top_bound - position.y;
+                dimensions.width = dimensions.height * image_selection.ratio;
+                position.x = selection_pos.x;
+            }
+            else
+            {
+                var new_height = dimensions.width / image_selection.ratio;
+                if (-1 !== reposition_y.indexOf(handle_index))
+                {
+                    position.y = selection_pos.y - (new_height - selection_rect.getHeight());
+                }
+                dimensions.height = new_height;
+            }
         }
         else
         {
-            var new_width = dimensions.height * image_selection.ratio;
-            if (-1 !== reposition_x.indexOf(handle_index))
+            if (position.x + dimensions.width >= boundry.right 
+                && (1 <= handle_index && 5 >= handle_index)
+                && selection_rect.getHeight() <= dimensions.height)
             {
-                position.x = selection_pos.x - (new_width - selection_rect.getWidth());
+                dimensions.width = boundry.right - position.x;
+                dimensions.height = dimensions.width / image_selection.ratio;
+                position.y = selection_pos.y;
             }
-            dimensions.width = new_width;
-        }
-
-        // Correct 
-        if (position.x + dimensions.width >= boundry.right)
-        {
-            dimensions.width = boundry.right - position.x;
-            dimensions.height = dimensions.width / image_selection.ratio;
-        }
-        else if (position.x <= boundry.left)
-        {
-            var right_bound = position.x + dimensions.width;
-            position.x = boundry.left;
-            dimensions.width = right_bound - position.x;
-            dimensions.height = dimensions.width / image_selection.ratio;
-        }
-        if (position.y + dimensions.height >= boundry.bottom)
-        {
-            dimensions.height = boundry.bottom - position.y;
-            dimensions.width = dimensions.height * image_selection.ratio;
-        }
-        else if (position.y <= boundry.top)
-        {
-            var bottom_bound = position.y + dimensions.height;
-            position.y = boundry.top;
-            dimensions.height = top_bound - position.y;
-            dimensions.width = dimensions.height * image_selection.ratio;
+            else if (position.x <= boundry.left 
+                && (-1 !== [0, 6, 7].indexOf(handle_index))
+                && selection_rect.getHeight() <= dimensions.height)
+            {
+                var right_bound = position.x + dimensions.width;
+                position.x = boundry.left;
+                dimensions.width = right_bound - position.x;
+                dimensions.height = dimensions.width / image_selection.ratio;
+                position.y = selection_pos.y;
+            }
+            else
+            {
+                var new_width = dimensions.height * image_selection.ratio;
+                if (-1 !== reposition_x.indexOf(handle_index))
+                {
+                    position.x = selection_pos.x - (new_width - selection_rect.getWidth());
+                }
+                dimensions.width = new_width;
+            }
         }
 
         return { pos: position, dim: dimensions };
