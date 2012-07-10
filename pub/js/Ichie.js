28a022c9f642633d232dd10807a1cecf0704ea9e
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-07-10 19:24:46 +0000

    fixed implementation for keeping the position and dimension result for the 'locked-ratio' calculation inside the image boundries.

--- pub/js/Ichie.js
+++ pub/js/Ichie.js
@@ -620,11 +620,10 @@
          */
         calculateSelectDimensions: function(handle_index, delta)
         {
-            var image_selection = this.interaction.getImageSelection();
+            var image_selection = this.interaction.getImageSelection(),
+                selection_rect = image_selection.getSelectionRect();
 
-            // Setup vars required for calculation.
-            var selection_rect = image_selection.getSelectionRect(),
-                width = selection_rect.getWidth(),
+            var width = selection_rect.getWidth(),
                 height = selection_rect.getHeight();
 
             var dir = ResizeInteraction.DIRECTION,
@@ -749,10 +748,12 @@
             direction = this.determineResizeDirection(handle_index, delta),
             dimensions = selection.dim,
             position = selection.pos,
+            boundry = this.interaction.getBoundry(),
             selection_rect = image_selection.getSelectionRect(),
             selection_pos = selection_rect.getAbsolutePosition();
 
-        var reposition_x = [0, 6], reposition_y = [0, 2];
+        // Calculate the position and dimensions depending on the given ration.
+        var reposition_x = [0, 6], reposition_y = [0, 2]; // handles that require repositioning of the select rect
         if (direction === dir.HORIZONTAL)
         {
             var new_height = dimensions.width / image_selection.ratio;
@@ -771,6 +772,33 @@
             }
             dimensions.width = new_width;
         }
+
+        // Correct 
+        if (position.x + dimensions.width >= boundry.right)
+        {
+            dimensions.width = boundry.right - position.x;
+            dimensions.height = dimensions.width / image_selection.ratio;
+        }
+        else if (position.x <= boundry.left)
+        {
+            var right_bound = position.x + dimensions.width;
+            position.x = boundry.left;
+            dimensions.width = right_bound - position.x;
+            dimensions.height = dimensions.width / image_selection.ratio;
+        }
+        if (position.y + dimensions.height >= boundry.bottom)
+        {
+            dimensions.height = boundry.bottom - position.y;
+            dimensions.width = dimensions.height * image_selection.ratio;
+        }
+        else if (position.y <= boundry.top)
+        {
+            var bottom_bound = position.y + dimensions.height;
+            position.y = boundry.top;
+            dimensions.height = top_bound - position.y;
+            dimensions.width = dimensions.height * image_selection.ratio;
+        }
+
         return { pos: position, dim: dimensions };
     };
 
